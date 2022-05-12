727adb40f8e29aa38bc45b518168419be9e1ffc3
Dante <dante142@gmail.com>
2022-05-12 01:38:44 -0700

    implemented RNG and picking that word from the file

--- dmhangman.asm
+++ dmhangman.asm
@@ -8,6 +8,7 @@
 	wordone: .space 33
 	wordtwo: .space 33
 	fn: .asciiz "/Users/dante/Documents/MARS/words.txt" #must include full filepath for words.txt
+		.align 2
 	wordlist: .space 1024
 	
 .text
@@ -32,6 +33,7 @@
 		la $a0, p1 #save the name in p1
 		li $a1, 32
 		syscall
+		j sp_word
 		
 		
 	name_MP: 
@@ -55,6 +57,7 @@
 		#li $v0, 4
 		#la $a0, p2
 		#syscall 
+		j multiplayer
 		
 	sp_word: #currently just loads the text file in, need to isolate a single word
 		li $v0, 13
@@ -62,17 +65,65 @@
 		li $a1, 0 #file flag
 		syscall
 		move $s0, $v0
-		li $v0, 14
+		li $v0, 14 #read in a file
 		move $a0, $s0
 		la $a1, wordlist
 		la $a2, 1024
 		syscall 
-		li $v0, 16
+		li $v0, 16 #close the file
 		move $a0, $s0
 		syscall
 		#li $v0, 4
 		#la $a0, wordlist
 		#syscall
+		li $v0, 42 #generate a random number with upper range in a1
+		li $a1, 83
+		syscall
+		#li $v0, 1
+		#syscall
+		move $t0, $a0 #put the random number into t0, we will pick the $t0'th word from the list of words
+		#li $t0, 0 #this is the word number to pick 
+		li $t1, 0 #counter for each byte in wordlist
+		li $t4, 0 #counter for how many words we have search so far
+		la $t5, wordone #space to store the selected word
+		beq $t0, 0, found_word #if the picked word is the first word we can skip to the found function
+		j isolate_word #we will parse the word list letter by letter
+		
+	isolate_word:
+		#li $v0, 11 #testing 
+		lb $a0, wordlist($t1) #load the byte at $t1
+		#syscall #testing
+		addiu $t1,$t1,1 #increment the counter ($1)
+		beq $a0, 10, newline #if we reach the end of a word, jump to newline function
+		j isolate_word #loop back around
+		
+	found_word:
+		#li $v0, 11 #testing
+		lb $a0, wordlist($t1) #load the byte
+		#syscall #testing
+		sb $a0, 0($t5) #store the byte
+		addiu $t5, $t5, 1 #increment pointer
+		addiu $t1, $t1, 1 #increment counter
+		beq $a0, 10, singleplayer #if we reach a newline we have finished parsing and storing the correct word
+		j found_word #loop
+		
+		
+	newline:
+		addiu $t4, $t4, 1 #increment the number of words we have parsed
+		beq $t4, $t0, found_word #if we have reached the word we want to use, jump to found_word function
+		j isolate_word #otherwise move to next word
+		
+		
+		
+	singleplayer:
+		#word is stored in wordone
+		li $v0, 4
+		la $a0, wordone
+		syscall
+		j menuloop
+		
+	multiplayer: 
+		j menuloop 
 			
 	j menuloop
 	
